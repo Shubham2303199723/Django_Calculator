be6f58964a87c950898a3b3381d9b7dfb8a7b050
shubham gurjar <shubhamgurjar222@gmail.com>
2024-04-11 08:45:19 +0530

    Django Calculator

---
+++ django_calculator/views.py
@@ -0,0 +1,24 @@
+from django.http import request
+from django.shortcuts import render 
+
+
+def index(request):
+    data={}
+    try:
+        number1 = int(request.GET['first_number'])
+        number2 = int(request.GET['Second_number'])
+        if request.GET['oprator'] == '+':
+            ans = number1 + number2
+        elif request.GET['oprator'] == '-':
+            ans = number1 - number2
+        elif request.GET['oprator'] == '*':
+            ans = number1 * number2
+        elif request.GET['oprator'] == '/':
+            ans = int(number1 / number2)
+        data={
+            'ans': ans
+        }
+        
+    except:
+        pass
+    return render(request, 'index.html', data)
